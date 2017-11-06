1ab0efa5b88165fac668aefc124e2e1744214b4c
bstelly <bstelly1999@gmail.com>
2017-11-06 16:59:47 -0600

    Linkedmath libraries

---
+++ dependencies/MathLib/Vector2.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+class Vector2
+{
+public:
+	float mX;
+	float mY;
+
+	Vector2();
+	Vector2(float x, float y);
+	Vector2 operator + (Vector2 & other);
+	Vector2 operator - (Vector2 & other);
+	Vector2 operator * (float other);
+	bool operator == (Vector2 &other);
+	float Dot(Vector2 &other);
+	Vector2 Normalize();
+	float Magnitude();
+	friend ostream& operator<<(ostream& os, const Vector2& vector);
+	friend istream & operator >> (istream &in, Vector2 vector);
+};
---
+++ dependencies/MathLib/Vector3.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <math.h>
+#include <iostream>
+using namespace std;
+class Vector3
+{
+public:
+	float mX;
+	float mY;
+	float mZ;
+
+	Vector3();
+	Vector3(float x, float y, float z);
+	Vector3 operator + (Vector3 & other);
+	Vector3 operator - (Vector3 & other);
+	Vector3 operator * (float other);
+	bool operator == (Vector3 & other);
+	float Dot(Vector3 & other);
+	Vector3 Cross(Vector3 & other);
+	Vector3 Normalize();
+	float Magnitude();
+	friend ostream& operator << (ostream& os, const Vector3& vector);
+	friend istream & operator >> (istream &in, Vector3 vector);
+
+};
